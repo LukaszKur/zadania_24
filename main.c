22cb0740ed5e0c255086af77783d855b2610cea2
LukaszKur <lukaszkurasz2@gmail.com>
2017-11-24 14:38:12 +0100

    Silnia

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int znak;
-    for (znak = 0; znak < 256; ++znak) {
-        printf("Wartosc całkowita: %d,\tkod ASCII %c\n",znak,znak);
-
+    int a,b,c;
+    b=1;
+    c=1;
+    printf("Podaj wartosc silni:\n");
+    scanf("%i",&a);
+    for (int i = 0; i < a; ++i)
+    {
+        c=c*b;
+        ++b;
     }
+    printf("Silnia liczby %i wynosi: %i",a,c);
     return 0;
 }
